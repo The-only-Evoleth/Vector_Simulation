e4f6beafc9ffad6868bbc2102764c29685c3d31f
Evoleth <megaarib@gmail.com>
2022-09-15 01:31:24 +0600

    insert all functions in the Point class

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 from utils import *
 
-a = Point(conv_to_unitv(np.array([[0,0,1]])))
-b = Point(conv_to_unitv(np.array([[-1,1,2]])))
-c = Point(conv_to_unitv(np.array([[3,-2,0]])))
-d = Point(conv_to_unitv(np.array([[-1,-2,-9]])))
-e = Point(conv_to_unitv(np.array([[1,1,0]])))
+a = Point(Point.conv_to_unitv(np.array([[0,0,1]])))
+b = Point(Point.conv_to_unitv(np.array([[-1,1,2]])))
+c = Point(Point.conv_to_unitv(np.array([[3,-2,0]])))
+#d = Point(conv_to_unitv(np.array([[-1,-2,-9]])))
+#e = Point(conv_to_unitv(np.array([[1,1,0]])))
 
 
 for i in range(100): #here range depicts how many times the sim will run. More points may take more steps to reach stable state
-    b.pos = newpos(b,1)
-    c.pos = newpos(c,1)
-    d.pos = newpos(d,1)
-    e.pos = newpos(e,1)
+    b.nextpos(1)
+    c.nextpos(1)
+ #   d.nextpos(1)
+ #   e.nextpos(1)
 #'a' is kept stable while other points move around a. This is to keep the end shape look better
 
 #prints out the final state after the simulation
-print(prettyfier(a.pos), "a")
-print(prettyfier(b.pos), "b")
-print(prettyfier(c.pos), "c")
-print(prettyfier(d.pos), "d")
-print(prettyfier(e.pos), "e")
+print(Point.prettyfier(a.pos), "a")
+print(Point.prettyfier(b.pos), "b")
+print(Point.prettyfier(c.pos), "c")
+#print(prettyfier(d.pos), "d")
+#print(prettyfier(e.pos), "e")
 
 
 
--- utils.py
+++ utils.py
@@ -7,53 +7,57 @@ class Point: #to make position vectors aka points have its properties
         self.initvel = initvel
         Point.all.append(self)
 
+    def test(self,t):
+        print(f"{self.pos} > Ima-live {t}")
 
+    @staticmethod
+    def distance(pos_1, pos_2): #distance between 2 points
+        d = np.matmul(pos_1-pos_2, np.transpose(pos_1-pos_2))
+        return d ** 0.5
 
-def distance(pos_1, pos_2): #distance between 2 points
-    d = np.matmul(pos_1-pos_2, np.transpose(pos_1-pos_2))
-    return d ** 0.5
+    def frc(self): #the force felt at a certain point or position
+        f = np.array([[0,0,0]])
+        for i in Point.all:
+            d = Point.distance(self.pos, i.pos)
+            if d != np.array([[0]]):
+                f = f + ((self.pos-i.pos)/d )*(1/d**2)
+        return f
 
-def frc(pt): #the force felt at a certain point or position
-    f = np.array([[0,0,0]])
-    for i in Point.all:
-        d = distance(pt, i.pos)
-        if d != np.array([[0]]):
-            f = f + ((pt-i.pos)/d )*(1/d**2)
-    return f
+    @staticmethod
+    def dot(a, b): #dot product of 2 position vectors
+        r = np.matmul((a*b),np.ones([3,1]))
+        return r
 
-def dot(a, b): #dot product of 2 position vectors
-    r = np.matmul((a*b),np.ones([3,1]))
-    return r
+    def tang_frc(self): #This is the force(tangent force) that will move the point 
+        f = self.frc()
+        r = f - ((self.pos* Point.dot(self.pos, f)) / (Point.distance(self.pos, [[0,0,0]]))**2 ) 
+        return r
 
-def tang_frc(pt): #This is the force(tangent force) that will move the point 
-    f = frc(pt)
-    r = f - ((pt* dot(pt, f)) / (distance(pt, [[0,0,0]]))**2 ) 
-    return r
+    def acc(self): #accelaretion
+        a = self.tang_frc()/1
+        return a
 
-def acc(pt): #accelaretion
-    a = tang_frc(pt)/1
-    return a
+    def nextpos(self, t): #next position of a point after a certain time(t)  
+        pos = self.pos
+        u = self.initvel
+        a = self.acc()
 
-def newpos(pt, t): #next position of a point after a certain time(t)  
-    pos = pt.pos
-    u = pt.initvel
-    a = acc(pt.pos)
+        s = (u*t) + ((a*(t**2))/2)
+        v = (u + (a*t))
+        self.initvel = v
+        newpos = (pos + s) * Point.distance(pos, [[0,0,0]])/Point.distance((pos+s), [[0,0,0]])    
+        self.pos = newpos
+        
+    @staticmethod
+    def conv_to_unitv(pos): #converts any vector to unit vector
+        r = pos/Point.distance(pos, [[0,0,0]])
+        return r
 
-    s = (u*t) + ((a*(t**2))/2)
-    v = (u + (a*t))
-    pt.initvel = v
-    newpos = (pos + s) * distance(pos, [[0,0,0]])/distance((pos+s), [[0,0,0]])    
-    return newpos
-
-
-def conv_to_unitv(pos): #converts any vector to unit vector
-    r = pos/distance(pos, [[0,0,0]])
-    return r
-
-def prettyfier(pos): # [[1,2,4]] -> {1,2,4}  
-    x = str(pos)[2:-2].split()
-    xf = ""
-    for i in x:
-        xf += ","+ i
-    xf = "{"+ xf[1:] +"}"
-    return xf
+    @staticmethod
+    def prettyfier(pos): # [[1,2,4]] -> {1,2,4}  
+        x = str(pos)[2:-2].split()
+        xf = ""
+        for i in x:
+            xf += ","+ i
+        xf = "{"+ xf[1:] +"}"
+        return xf
