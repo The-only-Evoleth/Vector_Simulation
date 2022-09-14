f1b44a3fe3ac6069e6faf9fda8f0929139708225
Evoleth <megaarib@gmail.com>
2022-09-15 01:40:20 +0600

    add def main().

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 from utils import *
 
-a = Point(Point.conv_to_unitv(np.array([[0,0,1]])))
-b = Point(Point.conv_to_unitv(np.array([[-1,1,2]])))
-c = Point(Point.conv_to_unitv(np.array([[3,-2,0]])))
-#d = Point(conv_to_unitv(np.array([[-1,-2,-9]])))
-#e = Point(conv_to_unitv(np.array([[1,1,0]])))
+
+def main():
+
+    a = Point(Point.conv_to_unitv(np.array([[0,0,1]])))
+    b = Point(Point.conv_to_unitv(np.array([[-1,1,2]])))
+    c = Point(Point.conv_to_unitv(np.array([[3,-2,0]])))
+    d = Point(Point.conv_to_unitv(np.array([[-1,-2,-9]])))
+    e = Point(Point.conv_to_unitv(np.array([[1,1,0]])))
 
 
-for i in range(100): #here range depicts how many times the sim will run. More points may take more steps to reach stable state
-    b.nextpos(1)
-    c.nextpos(1)
- #   d.nextpos(1)
- #   e.nextpos(1)
-#'a' is kept stable while other points move around a. This is to keep the end shape look better
+    for i in range(100): #here range depicts how many times the sim will run. More points may take more steps to reach stable state
+        b.nextpos(1)
+        c.nextpos(1)
+        d.nextpos(1)
+        e.nextpos(1)
+    #'a' is kept stable while other points move around a. This is to keep the end shape look better
 
-#prints out the final state after the simulation
-print(Point.prettyfier(a.pos), "a")
-print(Point.prettyfier(b.pos), "b")
-print(Point.prettyfier(c.pos), "c")
-#print(prettyfier(d.pos), "d")
-#print(prettyfier(e.pos), "e")
+    #prints out the final state after the simulation
+    print(Point.prettyfier(a.pos), "a")
+    print(Point.prettyfier(b.pos), "b")
+    print(Point.prettyfier(c.pos), "c")
+    print(Point.prettyfier(d.pos), "d")
+    print(Point.prettyfier(e.pos), "e")
+
+
+main()
 
 
 
